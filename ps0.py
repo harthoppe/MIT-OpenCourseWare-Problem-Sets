f4deb05ffa270f00ba0ccee156b91a5cd8f1f526
harthoppe <harthoppe@gmail.com>
2014-04-13 18:43:19 -0500

    Problem Set
    Course Number: 6.00SC

---
+++ ps0.py
@@ -0,0 +1,8 @@
+##Problem Set 0
+##Name: Hart Hoppe
+##Collaborators: N/A
+##Time Spent: 10mins
+
+birthday=str(raw_input("Enter your date of birth:"))
+lastname=str(raw_input("Enter your last name:"))
+print lastname,birthday
